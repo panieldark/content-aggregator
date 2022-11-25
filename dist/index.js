ddb292ddf5d7aa1f5d1a66eff5455c2af332fb85
danielpark <danielpark1140@gmail.com>
2022-11-25 18:09:57 -0500

    express hello world

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
     const fork = orm.em.fork();
+    const app = (0, express_1.default)();
+    app.get('/', (_, res) => {
+        res.send("Hello Daniel. Keep going!");
+    });
+    app.listen(4000, () => {
+        console.log("Server started on localhost:4000");
+    });
 };
 main().catch(err => {
     console.log(err);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { MikroORM } from "@mikro-orm/core"
 import {Post} from "./entities/Post";
 import microConfig from './mikro-orm.config';
+import express from 'express'
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
     const fork = orm.em.fork();
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send("Hello Daniel. Keep going!")
+    })
+    app.listen(4000, () => {
+        console.log("Server started on localhost:4000");
+    })
 }
 main().catch(err => {
     console.log(err)
